8e381a4843d18ea5e8177a4e55d4761098a0dc9f
Oleg <darek2587@gmail.com>
2024-02-12 21:40:37 +0300

    create repos

---
+++ src/test/java/geekbrains/glumov_oleg/GlumovOlegApplicationTests.java
@@ -0,0 +1,13 @@
+package geekbrains.glumov_oleg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlumovOlegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
